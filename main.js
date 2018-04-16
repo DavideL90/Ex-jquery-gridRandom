69a1601d560ba9d2650fac8752fbc95333d28c10
DavideL90 <davide.lecci@icloud.com>
2018-04-16 17:04:21 +0200

    checked if already clicked via js. Removed css check

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 $(document).ready(function(){
    //Call function to generate grid
    generateGrid();
+   //declare an array to contain the clicked cells
+   var clickedCells = [];
    //make an ajax request for a random number
    //when click on grid-cell
    $('.grid-cell').click(function(){
       //assign to a variable the value of cell
       var actualCell = $(this);
-      $.ajax({
-         url: 'https://www.boolean.careers/api/random/int',
-         method: 'GET',
-         success: function(data){
-            console.log(data.response);
-            var randomNumb = data.response;
-            if(data.response <= 5){
-               actualCell.addClass('yellow-backgr');
-               actualCell.text(randomNumb);
-               actualCell.addClass('disabled');
+      //Check if the clicked cell is already clicked
+      if(!clickedCells.includes(actualCell.index())){
+         //push the index of the cell into the array
+         clickedCells.push(actualCell.index());
+         $.ajax({
+            url: 'https://www.boolean.careers/api/random/int',
+            method: 'GET',
+            success: function(data){
+               var randomNumb = data.response;
+               if(data.response <= 5){
+                  actualCell.addClass('yellow-backgr');
+                  actualCell.text(randomNumb);
+
+               }
+               else{
+                  actualCell.addClass('green-backgr');
+                  actualCell.text(randomNumb);
+               }
+            },
+            error: function(){
 
             }
-            else{
-               actualCell.addClass('green-backgr');
-               actualCell.text(randomNumb);
-               actualCell.addClass('disabled');
-            }
-         },
-         error: function(){
-
-         }
-      });
+         });
+      }
 
    });
 });
