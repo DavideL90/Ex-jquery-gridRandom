ccca3fa93b3d42cc3bb6173138ff764bb3770965
DavideL90 <davide.lecci@icloud.com>
2018-04-16 14:46:23 +0200

    grid generated and style defined

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+   //Call function to generate grid
+   generateGrid();
+   //make an ajax request for a random number
    
 });
+
+//FUNZIONI
+//generate 6x6 grid
+function generateGrid(){
+   var gridCont = $('#grid-cnt');
+   for (var i = 0; i < 36; i++) {
+      gridCont.append('<div class="grid-cell">' + '</div>');
+   }
+}
